48a099132d8d58cde185bc444556a5ed08b3e526
Izahar <43846229+sonubit@users.noreply.github.com>
2026-08-31 15:32:51 +0530

    Implement CreditCardPayment strategy class

---
+++ src/main/java/behavioral/strategy/CreditCardPayment.java
@@ -0,0 +1,13 @@
+package behavioral.strategy;
+
+public class CreditCardPayment
+        implements PaymentStrategy {
+
+    @Override
+    public void pay(double amount) {
+
+        System.out.println(
+                "Paid $" + amount + " using Credit Card"
+        );
+    }
+}
