070797ab57dbad0e1bfac01b47f13cf65ab06226
Izahar <43846229+sonubit@users.noreply.github.com>
2026-08-31 15:53:54 +0530

    Add Employee class implementing Element interface

---
+++ src/main/java/behavioral/visitor/Employee.java
@@ -0,0 +1,25 @@
+package behavioral.visitor;
+
+public class Employee implements Element {
+
+    private final String name;
+    private final double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
