8757b34896b156334ce42510f4e8a1fdc6b8a880
Izahar <43846229+sonubit@users.noreply.github.com>
2026-08-31 15:56:10 +0530

    Implement NumberExpression class for interpreter pattern

---
+++ src/main/java/behavioral/interpreter/NumberExpression.java
@@ -0,0 +1,16 @@
+package behavioral.interpreter;
+
+public class NumberExpression
+        implements Expression {
+
+    private final int number;
+
+    public NumberExpression(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int interpret() {
+        return number;
+    }
+}
