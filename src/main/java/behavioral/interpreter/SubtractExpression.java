baa212a9152e61a82ebe4c6a9240946d8b44c944
Izahar <43846229+sonubit@users.noreply.github.com>
2026-08-31 15:56:59 +0530

    Implement SubtractExpression for interpreter pattern

---
+++ src/main/java/behavioral/interpreter/SubtractExpression.java
@@ -0,0 +1,23 @@
+package behavioral.interpreter;
+
+public class SubtractExpression
+        implements Expression {
+
+    private final Expression left;
+    private final Expression right;
+
+    public SubtractExpression(
+            Expression left,
+            Expression right) {
+
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+
+        return left.interpret()
+                - right.interpret();
+    }
+}
