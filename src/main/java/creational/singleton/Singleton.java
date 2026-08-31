c1390b3598ce1b19681cbab970d06e2edfadeb29
Izahar <43846229+sonubit@users.noreply.github.com>
2026-08-31 15:26:39 +0530

    Implement Singleton pattern in Singleton.java

---
+++ src/main/java/creational/singleton/Singleton.java
@@ -0,0 +1,28 @@
+package creational.singleton;
+
+public final class Singleton {
+
+    private static volatile Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+
+        if (instance == null) {
+
+            synchronized (Singleton.class) {
+
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void execute() {
+        System.out.println("Singleton instance executed.");
+    }
+}
