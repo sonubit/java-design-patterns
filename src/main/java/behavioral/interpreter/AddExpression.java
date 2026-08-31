0ea23d7d9cad11ad2644de6dbdb3073522e6a6f6
Izahar <43846229+sonubit@users.noreply.github.com>
2026-08-31 15:56:31 +0530

    Implement AddExpression class for interpretation

---
+++ src/main/java/behavioral/interpreter/AddExpression.java
@@ -0,0 +1,23 @@
+package behavioral.interpreter;
+
+public class AddExpression
+        implements Expression {
+
+    private final Expression left;
+    private final Expression right;
+
+    public AddExpression(
+            Expression left,
+            Expression right) {
+
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+
+        return left.interpret()
+                + right.interpret();
+    }
+}
