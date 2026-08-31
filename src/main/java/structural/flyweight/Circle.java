435c5c8d42471be85843ce5223bae3c1742d9d4b
Izahar <43846229+sonubit@users.noreply.github.com>
2026-08-31 15:49:19 +0530

    Implement Circle class in Flyweight pattern

---
+++ src/main/java/structural/flyweight/Circle.java
@@ -0,0 +1,19 @@
+package structural.flyweight;
+
+public class Circle implements Shape {
+
+    private final String color;
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void draw(int x, int y) {
+
+        System.out.println(
+                "Drawing " + color +
+                " circle at (" + x + ", " + y + ")"
+        );
+    }
+}
